85704497c4c6f28cedf3fdee6ecce7dc84f9fb87
Siarhei Tatarchanka <zlojdigger@gmail.com>
2023-10-26 16:16:43 +0200

    -added cmake example and compiler test

---
+++ examples/ReadWrite.cpp
@@ -0,0 +1,17 @@
+/******************************************************************************
+* File Name          : ReadWrite.cpp
+* Author             : Siarhei Tatarchanka
+* Version            : v 1.0
+* Description        : 
+*******************************************************************************/
+
+#include <iostream>
+
+#include "SerialDevice.hpp"
+
+
+int main(void)
+{
+    std::cout<<"compiler test"<<std::endl;
+    return 0;
+}
