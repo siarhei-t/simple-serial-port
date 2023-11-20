02704d79b8572ddfc7896dc8b75b575c7eac6a98
Siarhei Tatarchanka <zlojdigger@gmail.com>
2023-11-20 16:53:18 +0100

    -tested read/write with new config

--- examples/ReadWrite.cpp
+++ examples/ReadWrite.cpp
@@ -13,23 +13,30 @@ int main(void)
 {
     SerialPort* port;
     SerialDevice device;
+    const PortConfig config = 
+    {
+        .baudrate   = PortBaudRate::BD_9600,
+        .data_bits  = PortDataBits::DB_EIGHT,
+        .parity     = PortParity::P_NONE,
+        .stop_bits  = PortStopBits::SB_ONE,
+        .timeout_ms = 1000
+    };
     std::vector<std::string> devices;
 
     device.GetListOfAvailableDevices(devices);
-    
-    std::cout<<"*************************"<<std::endl;
+
     std::cout<<"List of available ports :"<<std::endl;
-    for(auto i = 0; i < devices.size();i++)
+    for(auto i = 0; i < devices.size(); i++)
     {
         std::cout<<"dev "<<int(i)<<" : "<<devices[i]<<std::endl;
     }
-    std::cout<<"*************************"<<std::endl;
-    
-    /*
     if(devices.size() > 0)
     {
-        std::cout<<"opening first available port..."<<std::endl;
-        device.CreatePortInstance(devices[0]);
+        int port_id;
+        std::cout<<"please select port to open..."<<std::endl;
+        std::cin>>port_id;
+        std::cout<<"trying to open port with id "<<int(port_id)<<std::endl;
+        device.CreatePortInstance(devices[port_id],config);
         port = device.GetPointerToPort();
     }
     else
@@ -37,7 +44,7 @@ int main(void)
         std::cout<<"There is no ports to work with."<<std::endl;
         return 0;
     }
-
+    
     if(port != nullptr)
     {
         std::string testLine = "This is a test string.";
@@ -62,6 +69,6 @@ int main(void)
         port->Close();
         
     }
-    */
+    
     return 0;
 }
